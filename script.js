50aada80bf4a49e33eaeb2358326bbf8fc19a147
UncleKorney <peychev.2005@mail.ru>
2025-11-16 21:47:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const sections = document.querySelectorAll("section");
-    const options = {
-        threshold: 0.5 // когда секция будет на 50% видна
-    };
-
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("visible");
-                entry.target.classList.add("fade-in");
-            }
-        });
-    }, options);
-
-    sections.forEach(section => {
-        observer.observe(section);
-    });
-
-    // Воспроизведение музыки по клику на страницу
-    const tracks = document.querySelectorAll('.track');
     const playPauseBtns = document.querySelectorAll('.play-pause-btn');
     const progressBars = document.querySelectorAll('.progress-bar');
 
-    playPauseBtns.forEach((button, index) => {
-        const audio = tracks[index].querySelector('audio');
+    playPauseBtns.forEach((button) => {
+        const trackNumber = button.getAttribute('data-track');
+        const audio = document.querySelector(`#track1-${trackNumber} audio`);
+
         button.addEventListener('click', () => {
             if (audio.paused) {
                 audio.play();
                 button.textContent = '⏸️'; // Изменение на паузу
-                updateProgressBar(audio, progressBars[index]);
+                updateProgressBar(audio, progressBars[trackNumber - 1]);
             } else {
                 audio.pause();
                 button.textContent = '▶️'; // Изменение на воспроизведение
@@ -46,17 +28,4 @@ document.addEventListener('DOMContentLoaded', function() {
             audio.currentTime = (progressBar.value / 100) * audio.duration;
         });
     }
-
-    // Функции анимации
-    function createWaterAnimation() {
-        const waterElement = document.createElement("div");
-        waterElement.classList.add("water");
-        document.body.appendChild(waterElement);
-    }
-    createWaterAnimation();
-
-    // Добавление фона с трещинами
-    const crackElement = document.createElement("div");
-    crackElement.classList.add("crack");
-    document.body.appendChild(crackElement);
 });
